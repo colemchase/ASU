84054071cf96882b3f939cdf2c73e03afd3f0691
colemchase <colemchase@gmail.com>
2025-08-28 20:15:52 -0400

    inverse array created

--- CSE551/Module1/codingAssignment.py
+++ CSE551/Module1/codingAssignment.py
@@ -10,24 +10,17 @@ W = [ [5, 1, 2, 4, 3],              # Employee preference list
      [4, 2, 5, 3, 1] ]
 N = 5                               # Number of department & employee
 
+# Inverse Array Creation
 inv = [[i for i in range(N)] for i in range(N)]
 for i in range(N):
     for j in range(N):
-        inv[i][j-1] = W[i][j]
-        
+        inv[i][W[i][j]-1] = j+1
 
 
-
-employee = {}
-
-# WRITE YOUR CODE HERE
-#get man from man pile 
-for department in D:
-    if len(department) > 0:
-        pick = department.pop(0)
-
+employee = {} # add them in as their number not the index
 
 # if man has a selection, propose
+
 # if woman available, match them and take then out their piles and place in match pile
 # if women not available steal
 
@@ -36,9 +29,9 @@ for department in D:
 
 
 
-''' --- Visualizing the result, Printing the output --- '''
-Names = [ ['HR', 'CRM', 'Admin', 'Research', 'Development'],      # Initialize the mapping of names
-         ['Adam', 'Bob', 'Clare', 'Diane', 'Emily'] ]
-print('Result is:-')
-for i in range(N):
-    print(Names[0][i], ":", Names[1][employee[i]-1])                # Map the result to the names
+# ''' --- Visualizing the result, Printing the output --- '''
+# Names = [ ['HR', 'CRM', 'Admin', 'Research', 'Development'],      # Initialize the mapping of names
+#          ['Adam', 'Bob', 'Clare', 'Diane', 'Emily'] ]
+# print('Result is:-')
+# for i in range(N):
+#     print(Names[0][i], ":", Names[1][employee[i]-1])                # Map the result to the names
